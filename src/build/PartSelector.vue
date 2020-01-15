6fcf376e94bd5d626a284ddff9c877759d96d29b
Alexandre Giroux <giroux.dion@gmail.com>
2020-01-15 09:02:46 -0500

    Basic routing

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" :title="selectedPart.type" />
+    <img @click="showPartInfo" :src="selectedPart.src" :title="selectedPart.type" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -44,6 +44,9 @@ export default {
     this.emitSelectedPart();
   },
   methods: {
+    showPartInfo() {
+      this.$router.push('/parts');
+    },
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
     },
@@ -91,6 +94,7 @@ export default {
 }
 .part img {
   width: 165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
